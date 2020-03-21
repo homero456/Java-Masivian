1ea5a5b2c790351423af2f33284e36e1114d80e0
vfabian <vfabian@hotmail.com>
2020-03-21 17:11:06 -0500

    se divide en metodos la clase Printer

--- EjercicioDos/src/Printer.java
+++ EjercicioDos/src/Printer.java
@@ -1,32 +1,49 @@
 
 public class Printer {
 	
+	private int cantPrimes;
+	private int RR;  //Numero de filas
+	private int CC;	  //Numero de columnas
+	
+	public Printer(int RR, int CC) {
+		this.CC=CC;
+		this.RR=RR;
+	}
+	
 	public void Start(int P[]) {
-		final int M = 1000;
-		int PAGENUMBER;
-		int PAGEOFFSET;
-		int ROWOFFSET;
-		int C;
-		final int RR = 50;
-		final int CC = 4;
-		
-		PAGENUMBER = 1;
-		PAGEOFFSET = 1;
-		while (PAGEOFFSET <= M) {
-			System.out.print("The First ");
-			System.out.print(Integer.toString(M));
-			System.out.print(" Prime Numbers === Page ");
-			System.out.print(Integer.toString(PAGENUMBER));
-			System.out.println("\n");
-			for (ROWOFFSET = PAGEOFFSET; ROWOFFSET <= PAGEOFFSET + RR - 1; ROWOFFSET++) {
-				for (C = 0; C <= CC - 1; C++)
-					if (ROWOFFSET + C * RR <= M)
-						System.out.printf("%10d", P[ROWOFFSET + C * RR]);
-				System.out.println();
-			}
-			System.out.println("\f");
+		this.cantPrimes = P.length -1;
+		int PAGENUMBER = 1;
+		int PAGEOFFSET = 1;
+		while (PAGEOFFSET <= cantPrimes) {
+			PrintHeader(cantPrimes,PAGENUMBER);
+			PrintBody(PAGEOFFSET,P);
+			PrintFoot();
 			PAGENUMBER++;
 			PAGEOFFSET += RR * CC;
 		}
 	}
+	
+	public void PrintHeader(int cantPrimes,int PAGENUMBER) {
+		System.out.print("The First ");
+		System.out.print(Integer.toString(cantPrimes));
+		System.out.print(" Prime Numbers === Page ");
+		System.out.print(Integer.toString(PAGENUMBER));
+		System.out.println("\n");
+	}
+	
+	public void PrintBody(int PAGEOFFSET, int P[]) {
+		int ROWOFFSET;
+		int C;
+		for (ROWOFFSET = PAGEOFFSET; ROWOFFSET <= PAGEOFFSET + RR - 1; ROWOFFSET++) {
+			for (C = 0; C <= CC - 1; C++)
+				if (ROWOFFSET + C * RR <= cantPrimes)
+					System.out.printf("%10d", P[ROWOFFSET + C * RR]);
+			System.out.println();
+		}
+	}
+	
+	public void PrintFoot() {
+		System.out.println("end page \f");
+	}
+	
 }
--- EjercicioDos/src/Test.java
+++ EjercicioDos/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GeneratePrimes gp = new GeneratePrimes();
-		Printer p = new Printer();
+		Printer p = new Printer(5,4);
 		var list= gp.GetPrimes();
 		p.Start(list);
 		
